34f8903dc37b4257f46fbb72aea18b39c20cd89a
Dego <48290509+anishkandel@users.noreply.github.com>
2025-03-23 22:32:16 +0545

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,9 +26,9 @@ app.use('/api/users', require('./routes/userRoute'));
 app.use('/api/orders', require('./routes/orderRoute'));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+ app.use(express.static(path.join(__dirname, "../frontend/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname,  "../frontend/build", "index.html"));
   });
 } else {
 app.get('/',(req, res)=>{
